cdff27703e0cb712fb1e7f34f6831e319db762e1
Dennis <dennis.galinsky@gmail.com>
2023-06-02 15:25:03 -0400

    Queen Class

---
+++ src/chess/Queen.java
@@ -0,0 +1,30 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queen extends ChessPiece {
+
+    public Queen(int posx, int posy) {
+        super("Queen", posx, posy);
+    }
+
+    @Override
+    public List<int[]> getNextPotentialPositions(String[][] pad) {
+        ArrayList<int[]> potentialPositions = new ArrayList<>();
+        potentialPositions.addAll(super.getHorizantalVerticalPositions(pad));
+        potentialPositions.addAll(super.getDiagonalPositoins((pad)));
+        return potentialPositions;
+    }
+
+    public static void main(String[] args) {
+        String[][] pad = new String[][] {{"1", "2", "3"},{"4", "5", "6"},{"7", "8", "9"},{"*", "0", "#"}};
+        Queen queen = new Queen(1,0);
+        System.out.println(pad[queen.getPosy()][queen.getPosx()]);
+        System.out.println();
+        List<int[]> list = queen.getNextPotentialPositions(pad);
+        for (int[] pair : list) {
+            System.out.println(pad[pair[1]][pair[0]]);
+        }
+    }
+}
