14fe39ee62161fc6a5c769c209a9b2cddd047dbf
Dennis <dennis.galinsky@gmail.com>
2023-06-03 14:23:24 -0400

    phone number generator wip

---
+++ src/telephone/PhoneNumberGenerator.java
@@ -0,0 +1,68 @@
+package telephone;
+
+import chess.*;
+
+import java.util.List;
+
+public class PhoneNumberGenerator {
+
+    private String[][] phonePad;
+    private String initialKey;
+
+    public PhoneNumberGenerator(String[][] phonePad, String initialKey) {
+        this.phonePad = phonePad;
+        this.initialKey = initialKey;
+    }
+
+    private int[] getInitialCoordinates(){
+        for(int i = 0; i<phonePad.length; i++){
+            for(int j=0; j<phonePad[i].length; j++){
+                if(phonePad[i][j].equals(initialKey)){
+                    return new int[] {j, i};
+                }
+            }
+        }
+        return null;
+    }
+
+    private int generatePhoneNumbers(){
+        int[] initialCoordinates = getInitialCoordinates();
+        assert initialCoordinates != null;
+        ChessPiece piece = new Rook(initialCoordinates[0], initialCoordinates[1]);
+        int total = generatePhoneNumbersForPiece(piece, initialKey);
+        System.out.println(total);
+        return total;
+    }
+
+    private int generatePhoneNumbersForPiece(ChessPiece piece, String phoneNumber){
+        if(phoneNumber.length() == PhoneNumber.VALIDLENGTH){
+            System.out.println(phoneNumber);
+            return 1;
+        }
+
+        List<int[]> nextPossiblePositions = piece.getNextPotentialPositions(phonePad);
+        if(nextPossiblePositions.size() == 0){
+            return 0;
+        }
+
+        int sum = 0;
+
+        for(int[] pair : nextPossiblePositions){
+            piece.setPosx(pair[0]);
+            piece.setPosy(pair[1]);
+            String nextNum = phonePad[piece.getPosy()][piece.getPosx()];
+            sum += generatePhoneNumbersForPiece(piece, phoneNumber + nextNum);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String[][] pad = new String[][] {{"1", "2", "3"},{"4", "5", "6"},{"7", "8", "9"},{"*", "0", "#"}};
+        PhoneNumberGenerator generator = new PhoneNumberGenerator(pad, "1");
+        generator.generatePhoneNumbers();
+    }
+
+
+
+
+}
